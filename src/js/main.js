a53a812fd17babd84b629483a2f9b4f7acde8a0f
Vladimir <Goldgraalar@gmail.com>
2017-12-14 23:21:21 +0200

    first init

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,83 @@
  */
 //= ../../bower_components/jquery/dist/jquery.min.js
 
+;(function() {
+'use strict';
 /*
     Custom
  */
 //= partials/helper.js
+
+
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+// c.fillStyle = "rgba(255, 0, 0, 0.5)";
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = "rgba(0, 0, 255, 0.5)";
+// c.fillRect(200, 200, 100, 100);
+// c.fillStyle = "rgba(0, 255, 0, 0.5)";
+// c.fillRect(300, 300, 100, 100);
+
+// Line
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "#fa34a3";
+// c.stroke();
+
+
+// Arc / Circle
+// c.beginPath();
+// c.arc(300, 300, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = 'blue';
+// c.stroke();
+
+// for (var i = 0; i < 3; i++) {
+//   var x = Math.random() * window.innerWidth;
+//   var y = Math.random() * window.innerHeight;
+//   c.beginPath();
+//   c.arc(x, y, 30, 0, Math.PI * 2, false);
+//   c.strokeStyle = 'blue';
+//   c.stroke();
+// }
+
+
+
+
+var x = Math.random() * window.innerWidth;
+var y = Math.random() * window.innerHeight;
+var dx = (Math.random() - 0.5) * 10;
+var dy = (Math.random() - 0.5) * 5;
+var radius = 30;
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+  c.beginPath();
+  c.arc(x, y, radius, 0, Math.PI * 2, false);
+  c.strokeStyle = 'blue';
+  c.stroke();
+
+
+  if (x + radius > innerWidth || x - radius < 0) {
+    dx = -dx;
+  }
+
+  if (y + radius > innerHeight || y - radius < 0) {
+    dy = -dy;
+  }
+  x += dx;
+  y += dy;
+};
+
+animate();
+
+})();
+
+
+
